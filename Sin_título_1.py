ee0a1044b00c99bd4ec220ce80cf86bb4bfad9fd
Jesus Angulo Rivilla <jesus.angulo1008@gmail.com>
2019-10-12 22:32:25 +0000

    Update Sin_título_1.py

--- Sin_título_1.py
+++ Sin_título_1.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+"""
+Función con el Algoritmo en lenguaje Python
+
+No presenta problemas por debajo de 100.000 o más
+"""
+
 import collections
 
 s = set([1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97])
